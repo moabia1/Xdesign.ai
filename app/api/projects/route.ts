0abea3ddf58b5fb6216bcc94916583b19597e78d
moabia1 <mdmoabia81@gmail.com>
2026-01-19 22:36:40 +0530

    mongodb set-up with prisma completed

---
+++ app/api/projects/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/prisma"
+import { useUser } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+
+export async function POST(request: Request) {
+  try {
+    const { prompt } = await request.json()
+    const { user } = useUser()
+
+    if (!user) throw new Error("Unauthorized")
+    if (!prompt) throw new Error("Missing prompt")
+    
+    const userId = user.id
+
+    //const projectName = await generateProjectName(prompt)
+
+    const project = await prisma.project.create({
+      data: {
+        userId,
+        name:""
+      }
+    })
+
+    return NextResponse.json({
+      success: true,
+      data:project
+    })
+  } catch (error) {
+    console.log("Error occured :", error)
+    return NextResponse.json({
+      error: "Failed to create project"
+    }, { status: 500 })
+  }
+}
