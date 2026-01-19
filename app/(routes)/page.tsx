3c143d52be2ba08c7de7fcf66e07f0e8fe901eb5
moabia1 <mdmoabia81@gmail.com>
2026-01-19 14:17:47 +0530

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LandingSection from "./_common/landing-section";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingSection/>
+    </div>
+  );
+}
